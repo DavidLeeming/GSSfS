a462785ce7046504ee9fbcb135737580d60be72a
DavidLeeming <90982717+DavidLeeming@users.noreply.github.com>
2022-03-04 15:49:30 +0000

    pivot table script

--- pivot_table.py
+++ pivot_table.py
@@ -1,19 +1,19 @@
-import datetime, pandas as pd, numpy as np, re, itertools, threading, time, sys, pathlib
+import datetime, pandas as pd, numpy as np, re, itertools, threading, time, sys, pathlib, xlsxwriter
+from logging import exception
 from pathlib import Path
 import win32com.client as win32
 from datetime import date, timedelta
 win32c = win32.constants
-
+cwd = (str(sys.argv[0][:-21]))
+print(cwd)
 
 def pivot_table(wb: object, ws1: object, pt_ws: object, ws_name: str, pt_name: str, pt_rows: list, pt_filters: list, pt_fields: list):
-    """
-    wb = workbook1 reference
-    ws1 = worksheet1 that contain the data
-    pt_ws = pivot table worksheet number
-    ws_name = pivot table worksheet name
-    pt_name = name given to pivot table
-    pt_rows, pt_cols, pt_filters, pt_fields: values selected for filling the pivot tables
-    """
+    #wb = workbook1 reference
+    #ws1 = worksheet1 that contain the data
+    #pt_ws = pivot table worksheet number
+    #ws_name = pivot table worksheet name
+    #pt_name = name given to pivot table
+    #pt_rows, pt_cols, pt_filters, pt_fields: values selected for filling the pivot tables
 
     # pivot table location
     pt_loc = len(pt_filters) + 2
@@ -43,9 +43,10 @@ def pivot_table(wb: object, ws1: object, pt_ws: object, ws_name: str, pt_name: s
     pt_ws.PivotTables(pt_name).ShowValuesRow = True
     pt_ws.PivotTables(pt_name).ColumnGrand = True
 
+
 def run_excel(f_path: Path, f_name: str, sheet_name: str):
     
-    filename = f_path / f_name
+    filename = str(f_path) + '/' + str(f_name)
 
     # create excel object
     excel = win32.gencache.EnsureDispatch('Excel.Application')
@@ -54,17 +55,10 @@ def run_excel(f_path: Path, f_name: str, sheet_name: str):
     excel.Visible = True  # False
     
     # try except for file / path
-    try:
-        wb = excel.Workbooks.Open(filename)
-    except com_error as e:
-        if e.excepinfo[5] == -2146827284:
-            print(f'Failed to open spreadsheet.  Invalid filename or location: {filename}')
-        else:
-            raise e
-        sys.exit(1)
+    wb = excel.Workbooks.Open(filename)
 
     # set worksheet
-    ws1 = wb.Sheets('Sales')
+    ws1 = wb.Sheets('Eng Not recruited')
     
     # Setup and call pivot_table
     ws2_name = 'pivot_table'
@@ -72,16 +66,16 @@ def run_excel(f_path: Path, f_name: str, sheet_name: str):
     ws2 = wb.Sheets(ws2_name)
     
     # update the pt_name, pt_rows, pt_cols, pt_filters, pt_fields at your preference
-    pt_name = 'example'  # pivot table name, must be a string
-    pt_rows = ['Genre']  # rows of pivot table, must be a list
+    pt_name = 'England'  # pivot table name, must be a string
+    pt_rows = ['Establishment name']  # rows of pivot table, must be a list
     # pt_cols = []  # columns of pivot table, must be a list
-    pt_filters = ['Year']  # filter to be applied on pivot table, must be a list
+    pt_filters = ['Region']  # filter to be applied on pivot table, must be a list
     # [0]: field name [1]: pivot table column name [3]: calulation method [4]: number format (explain the list item of pt_fields below)
-    pt_fields = [['North America', 'Total Sales in North America', win32c.xlSum, '0'],  # must be a list of lists
-                 ['Europe', 'Total Sales in Europe', win32c.xlSum, '0'],
-                 ['Japan', 'Total Sales in Japan', win32c.xlSum, '0'],
-                 ['Rest of World', 'Total Sales in Rest of World', win32c.xlSum, '0'],
-                 ['Global', 'Total Global Sales', win32c.xlSum, '0']]
+    pt_fields = [['Postcode', 'Postcode1', win32c.xlCount, '0'],  # must be a list of lists
+                 ['Phase of education', 'Phase of education1', win32c.xlSum, '0'],
+                 ['Website address', 'Website address1', win32c.xlSum, '0'],
+                 ['Telephone number', 'Telephone number1', win32c.xlSum, '0']
+                 ]
     # calculation method: xlAverage, xlSum, xlCount
     pivot_table(wb, ws1, ws2, ws2_name, pt_name, pt_rows, pt_filters, pt_fields)
     wb.Save() # save the pivot table created
@@ -91,14 +85,19 @@ def run_excel(f_path: Path, f_name: str, sheet_name: str):
 
 def main():
     # sheet name for data
-    sheet_name = 'Sales'  # update with sheet name from your file
+    sheet_name = 'Eng Not recruited'  # update with sheet name from your file
     # file path
-    f_path = Path.cwd()  # file in current working directory
+    f_path = cwd  # file in current working directory
 #   f_path = Path(r'c:\...\Documents')  # file located somewhere else
     # excel file
-    f_name = 'PS4_GamesSales.xlsx' # change to your Excel file name
+    f_name = 'output.xlsx' # change to your Excel file name
     
     # function calls
     run_excel(f_path, f_name, sheet_name)
-cwd = (str(sys.argv[0][:-15]))
-print(cwd)
+
+import openpyxl as px
+
+wb= px.load_workbook(str(cwd) + '/output.xlsx')
+ws = wb["Eng Not recruited"]
+ws.auto_filter.ref = ws.dimensions
+wb.save(str(cwd) + '/output.xlsx')
